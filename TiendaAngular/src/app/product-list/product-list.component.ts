a982b80ac441594e457ff65c9d566e894da3235a
SORIUQ <jorgepenalverquiros@gmail.com>
2025-02-13 19:19:17 +0100

    Proyecto TiendaAngular V1

---
+++ TiendaAngular/src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { products } from '../products';
+import { RouterLink } from '@angular/router';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [RouterLink, CommonModule, ProductAlertsComponent],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  products = [...products];
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
