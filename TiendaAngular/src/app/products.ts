fd56713f4aae1b98c091f19231c46349bdb6439b
Jorge Peñalver <jorgepenalverquiros@gmail.com>
2025-02-18 10:58:00 +0100

    Cambios examen shop - Jorge Peñalver Quiros

--- TiendaAngular/src/app/products.ts
+++ TiendaAngular/src/app/products.ts
@@ -3,6 +3,12 @@ export interface Product {
     name: string;
     price: number;
     description: string;
+    stock: number,
+    caracteristicas?: string[],
+    garantia?: {
+      periodo: string;
+      tipo: string;
+    };
   }
   
   export const products = [
@@ -10,18 +16,55 @@ export interface Product {
       id: 1,
       name: 'Phone XL',
       price: 799,
-      description: 'A large phone with one of the best screens'
+      description: 'A large phone with one of the best screens',
+      stock: 10,
+      caracteristicas: ["5G", "Pantalla OLED", "Almacenamiento de 512GB"],
+      garantia: {
+        periodo: "3 años",
+        tipo: "Fabricante"
+      }
     },
     {
       id: 2,
       name: 'Phone Mini',
       price: 699,
-      description: 'A great phone with one of the best cameras'
+      description: 'A great phone with one of the best cameras',
+      stock: 3,
+      caracteristicas: ["4G", "Pantalla normal", "Almacenamiento de 256GB"],
+      garantia: {
+        periodo: "2 años",
+        tipo: "Fabricante"
+      }
     },
     {
       id: 3,
       name: 'Phone Standard',
       price: 299,
-      description: ''
+      description: '',
+      stock: 0,
+      caracteristicas: ["3G", "Pantalla barata", "Almacenamiento de 128GB"],
+      garantia: {
+        periodo: "1 años",
+        tipo: "Fabricante"
+      }
+    },
+    {
+      id: 4,
+      name: 'Sin caracteristicas, stock 5, con periodo en garantia',
+      price: 299,
+      description: 'TEXTO DESCRIPCION',
+      stock: 5,
+      garantia: {
+        periodo: "1 años",
+        tipo: "Fabricante"
+      }
+    },
+    {
+      id: 5,
+      name: 'Con caracteristicas, stock 4, sin periodo en garantia',
+      price: 299,
+      description: 'TEXTO DESCRIPCION',
+      stock: 4,
+      caracteristicas: ["Caracteristica1", "Caracteristica2"]
     }
   ];
